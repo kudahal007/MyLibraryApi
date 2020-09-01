fde663ac76fe2407e3b71a360dbdc12a6a0b6444
Kumar Dahal <kudahal007@gmail.com>
2020-09-01 14:43:43 -0400

    Project start

---
+++ src/main/java/com/np/MyLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.np;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLibraryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/np/MyLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.np;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
